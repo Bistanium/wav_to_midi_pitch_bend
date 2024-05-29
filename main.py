b3d9f2b76ec1718dfcce768fc4ec0fc20715be9b
Bistam <63497549+Bistanium@users.noreply.github.com>
2024-05-30 00:30:30 +0900

    Update main.py
    アップサンプリングを追加。
    これにより、サンプリング周波数による再生時間の違いを吸収。
    
    一部変数名の変更

--- main.py
+++ main.py
@@ -4,57 +4,59 @@ from math import log10, modf
 import wave
 import numpy as np
 from numpy.fft import fft
+from scipy import interpolate
 from scipy.signal import firwin, lfilter
 
 # midi化関数
 def data2midi(F, fs, N):
+    absF = abs(F.imag)
     half_n = N // 2
     sec = N / fs
-    soundtime = int(round(120*sec, 0))
+    soundtime = int(round(120 * sec, 0))
     beforenote, maxvolume = 0, 0
     otolist = [[35] for _ in range(10)]
+
     # pitchbend
     pitches = [0, 410, 819, 1229, 1638, -2048, -1638, -1229, -819, -410]
     for i in range(1, 10):
-        channel = i if i != 9 else 10
-        tracks[i].append(Message('pitchwheel', channel=channel, pitch=pitches[i]))
+        ch = i if i != 9 else 10
+        tracks[i].append(Message('pitchwheel', channel=ch, pitch=pitches[i]))
 
     for i in range(1, half_n):
-        if abs(F.imag[i]/N) > 4: # 音量の閾値 無いと負荷でマズイ
-            i_sec = i/sec # 周波数
+        if absF[i] / N > 4: # 音量の閾値 無いと負荷でマズイ
+            i_sec = i / sec # 周波数
             if 64 < i_sec < 11175: # midiの範囲に収める
                 # ノート番号計算
-                midinote = 69 + log10(i_sec/440)/0.025085832972
+                midinote = 69 + log10(i_sec / 440) / 0.025085832972
                 # 音量計算
-                volume = (abs(F.imag[i]/N*2) ** (1.8/3))
+                volume = ((absF[i] / N * 2) ** (1.8 / 3))
                 if volume > 127: volume = 127
 
-                incomp_rounded_midinote = round(midinote, 1)
-                if beforenote != incomp_rounded_midinote: # 音が変わったら前の音階をmidiに打ち込む
+                incomp_rounded_note = round(midinote, 1)
+                if beforenote != incomp_rounded_note: # 音が変わったら前の音階をmidiに打ち込む
                     syosu, seisu = modf(beforenote) # 整数部分と小数部分の分離
                     syosu = round(syosu, 1)
-                    rounded_midinote = int(round(beforenote, 0))
-                    rounded_volume = int(round(maxvolume, 0))
+                    rounded_note, rounded_volume = int(round(beforenote, 0)), int(round(maxvolume, 0))
                     if syosu == 0.5:
-                        if beforenote - rounded_midinote > 0: # 四捨五入(38.5→38)になるとき
-                            rounded_midinote = rounded_midinote + 1 # 39にして正しい四捨五入にする
-                        if rounded_midinote in otolist[5]: #重複チェック
+                        if beforenote - rounded_note > 0: # 四捨五入(38.5→38)になるとき
+                            rounded_note = rounded_note + 1 # 39にして正しい四捨五入にする
+                        if rounded_note in otolist[5]: # 重複チェック
                             continue
-                    otonum = int(syosu*10) #トラックと対応させる
-                    ch = otonum if otonum != 9 else 10
-                    otolist[otonum].append(rounded_midinote)
-                    tracks[otonum].append(Message('note_on', note=rounded_midinote, velocity=rounded_volume, channel=ch, time=00))
-                    beforenote, maxvolume = incomp_rounded_midinote, volume
+                    tracknum = int(syosu*10) #トラックと対応させる
+                    ch = tracknum if tracknum != 9 else 10
+                    otolist[tracknum].append(rounded_note)
+                    tracks[tracknum].append(Message('note_on', note=rounded_note, velocity=rounded_volume, channel=ch, time=00))
+                    beforenote, maxvolume = incomp_rounded_note, volume
                 elif volume > maxvolume: # 同じ音階なら音量を今までの最大値にする
                     maxvolume = volume
 
     for i, track in enumerate(tracks):
         ch = i if i != 9 else 10
-        for j, midinote in enumerate(otolist[i]):
+        for j, note in enumerate(otolist[i]):
             if j == 0:
-                track.append(Message('note_off', note=midinote, channel=ch, time=soundtime))
+                track.append(Message('note_off', note=note, channel=ch, time=soundtime))
             else:
-                track.append(Message('note_off', note=midinote, channel=ch, time=0))
+                track.append(Message('note_off', note=note, channel=ch, time=0))
 
 
 # Wave読み込み
@@ -95,7 +97,7 @@ def audio_split(data, win_size):
     splited_data = []
     len_data = len(data)
     win = np.hanning(win_size)
-    for i in range(0, len_data, win_size//2):
+    for i in range(0, len_data, win_size // 2):
         endi = i + win_size
         if endi < len_data:
             splited_data.append(data[i:endi] * win)
@@ -105,17 +107,36 @@ def audio_split(data, win_size):
     return splited_data
 
 
-def downsampling(conversion_rate, data, fs):
+def upsampling(up_fs, data, fs):
     # FIRフィルタ
-    nyqF = fs/2                       # 変換前のナイキスト周波数
-    cF = (conversion_rate/2-500)/nyqF # カットオフ周波数
-    taps = 511                        # フィルタ係数
-    b = firwin(taps, cF)   # LPF
+    nyqF = fs / 2             # 変換前のナイキスト周波数
+    cF = (nyqF - 500) / nyqF  # カットオフ周波数
+    taps = 511                # フィルタ係数
+    b = firwin(taps, cF)      # LPF
+
+    # 補間処理
+    x = np.arange(0, len(data))
+    interpolated = interpolate.interp1d(x, data, kind="cubic")
+    upsampled_length = int(round(len(data) * up_fs / fs, 0))
+    uplate = np.linspace(0, len(data) - 1, upsampled_length)
+    uped_data = interpolated(uplate)
+
+    # フィルタリング
+    uped_data = lfilter(b, 1, uped_data)
+    return uped_data
+
+
+def downsampling(down_fs, data, fs):
+    nyqF = fs / 2
+    cF = (down_fs / 2 - 500) / nyqF
+    taps = 511
+    b = firwin(taps, cF)
     # フィルタリング
     data = lfilter(b, 1, data)
 
     # 間引き処理
-    downlate = np.arange(0, len(data)-1, fs/conversion_rate)
+    downsampled_length = int(round(len(data) * down_fs / fs, 0))
+    downlate = np.linspace(0, len(data) - 1, downsampled_length)
     rounded_indices = np.round(downlate).astype(int)
     downed_data = data[rounded_indices]
     return downed_data
@@ -131,26 +152,28 @@ if __name__ == '__main__':
     tracks[0].append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(480)))
 
     # Wav読み込み
-    data_l = read_wav("test.wav")
+    data = read_wav("test.wav")
 
     # Wavの情報取得
     wi = info_wav("test.wav")
 
     # ダウンサンプリング
-    if wi["fs"] > 40960:
-        new_fs = 40960
-        downed_data = downsampling(new_fs, data_l, wi["fs"])
-    else: #アップサンプリング予定
+    new_fs = 40960
+    if wi["fs"] > new_fs:
+        samped_data = downsampling(new_fs, data, wi["fs"])
+    elif wi["fs"] < new_fs:
+        samped_data = upsampling(new_fs, data, wi["fs"])
+    else:
         new_fs = wi["fs"]
-        downed_data = data_l
-    del data_l
+        samped_data = data
+    del data
 
     # ウィンドウサイズ
     win_size = 1024 * 16
 
     # データ分割
-    splited_data = audio_split(downed_data, win_size)
-    del downed_data
+    splited_data = audio_split(samped_data, win_size)
+    del samped_data
 
     # FFT&midi化
     len_splited_data = len(splited_data)
